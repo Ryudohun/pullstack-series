4d3ab7565c5318ddba1c2857684bf2fe83079558
dohun Ryu <80663475+Ryudohun@users.noreply.github.com>
2025-06-08 15:31:52 +0900

    Create models.py

---
+++ models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    title = db.Column(db.String(120))
+    description = db.Column(db.Text)
+    date = db.Column(db.Date)
+    time = db.Column(db.Time)
