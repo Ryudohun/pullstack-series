e0e337e1c8ea039858bd3b71538b2ebb6a38e0d9
dohun Ryu <80663475+Ryudohun@users.noreply.github.com>
2025-06-09 00:11:45 +0900

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,21 +1,46 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField, DateField, TimeField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
-class RegisterForm(FlaskForm):
-    username = StringField('아이디', validators=[DataRequired()])
-    email = StringField('이메일', validators=[DataRequired(), Email()])
-    password = PasswordField('비밀번호', validators=[DataRequired(), Length(min=6)])
+# 회원가입 폼
+class RegistrationForm(FlaskForm):
+    username = StringField('사용자 이름', validators=[
+        DataRequired(message='사용자 이름을 입력해주세요.'),
+        Length(min=2, max=20, message='이름은 2자 이상 20자 이하로 입력해주세요.')
+    ])
+    email = StringField('이메일', validators=[
+        DataRequired(message='이메일을 입력해주세요.'),
+        Email(message='올바른 이메일 형식을 입력해주세요.')
+    ])
+    password = PasswordField('비밀번호', validators=[
+        DataRequired(message='비밀번호를 입력해주세요.'),
+        Length(min=6, message='비밀번호는 최소 6자 이상이어야 합니다.')
+    ])
+    confirm_password = PasswordField('비밀번호 확인', validators=[
+        DataRequired(message='비밀번호 확인을 입력해주세요.'),
+        EqualTo('password', message='비밀번호가 일치하지 않습니다.')
+    ])
     submit = SubmitField('회원가입')
 
+# 로그인 폼
 class LoginForm(FlaskForm):
-    username = StringField('아이디', validators=[DataRequired()])
-    password = PasswordField('비밀번호', validators=[DataRequired()])
+    email = StringField('이메일', validators=[
+        DataRequired(message='이메일을 입력해주세요.'),
+        Email(message='올바른 이메일 형식을 입력해주세요.')
+    ])
+    password = PasswordField('비밀번호', validators=[
+        DataRequired(message='비밀번호를 입력해주세요.')
+    ])
     submit = SubmitField('로그인')
 
+# 일정 추가 폼
 class ScheduleForm(FlaskForm):
-    title = StringField('제목', validators=[DataRequired()])
-    description = TextAreaField('내용', validators=[DataRequired()])
-    date = DateField('날짜', validators=[DataRequired()])
-    time = TimeField('시간', validators=[DataRequired()])
+    title = StringField('제목', validators=[DataRequired(message='제목을 입력해주세요.')])
+    description = TextAreaField('설명')
+    date = DateField('날짜', format='%Y-%m-%d', validators=[
+        DataRequired(message='날짜를 입력해주세요. (예: 2025-06-08)')
+    ])
+    time = TimeField('시간', format='%H:%M', validators=[
+        DataRequired(message='시간을 입력해주세요. (예: 14:00)')
+    ])
     submit = SubmitField('일정 추가')
