15f910db127e0431690b4bbacaca87727a19363b
dohun Ryu <80663475+Ryudohun@users.noreply.github.com>
2025-06-08 23:16:23 +0900

    Update email_utils.py

--- email_utils.py
+++ email_utils.py
@@ -1,1 +1,9 @@
+from flask_mail import Mail, Message
 
+mail = Mail()
+
+def send_schedule_email(recipient, schedule):
+    msg = Message(f"[일정 알림] {schedule.title}",
+                recipients=[recipient])
+    msg.body = f"내용: {schedule.description}\n날짜: {schedule.date} {schedule.time}"
+    mail.send(msg)
